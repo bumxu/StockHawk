0d28963c00e90e8d45f8b7f5d8bf86e000025a31
Juande Martos <admin.lum@gmail.com>
2017-03-31 01:11:24 +0200

    Playing with MPAndroidChart.
    Added the Stock Details Activity and testing the charting library.

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -0,0 +1,137 @@
+package com.udacity.stockhawk.ui;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.Loader;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.udacity.stockhawk.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Pair<Date, Float>>> {
+    private static final int HISTORICAL_DATA_LOADER = 1;
+
+    @BindView(R.id.chart) LineChart mStockChart;
+
+    private String mSymbol;
+
+//    @Override
+//    public void onClick(String symbol) {
+//        Timber.d("Symbol clicked: %s", symbol);
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+
+        mSymbol = getIntent().getStringExtra("symbol");
+
+        getSupportActionBar().setTitle(mSymbol);
+
+        LoaderManager loaderManager = getSupportLoaderManager();
+        if (loaderManager.getLoader(HISTORICAL_DATA_LOADER) == null) {
+            loaderManager.initLoader(HISTORICAL_DATA_LOADER, null, this);
+        } else {
+            loaderManager.restartLoader(HISTORICAL_DATA_LOADER, null, this);
+        }
+    }
+
+    private void plot(final List<Pair<Date, Float>> data) {
+        // Prepare dataset
+
+        final List<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < data.size(); i++) {
+            final Float price = data.get(i).second;
+
+            entries.add(new Entry(i, price));
+        }
+
+        final LineDataSet dataSet = new LineDataSet(entries, "value");
+        dataSet.setDrawCircles(false);
+        dataSet.setLineWidth(1.5f);
+        dataSet.setDrawValues(false);
+        dataSet.setColor(ContextCompat.getColor(this, R.color.colorAccent));
+
+        // X Axis
+
+        final XAxis xAxis = mStockChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setGranularity(1f);
+        xAxis.setDrawGridLines(true);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            private SimpleDateFormat mFormat = new SimpleDateFormat("dd/MM/YY");
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mFormat.format(data.get((int) value).first);
+            }
+        });
+
+        // Y Axes
+
+        final YAxis leftAxis = mStockChart.getAxisLeft();
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(170f);
+
+        final YAxis rightAxis = mStockChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        // Setup
+
+        final LineData lineData = new LineData(dataSet);
+
+        mStockChart.setData(lineData);
+        mStockChart.invalidate();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<List<Pair<Date, Float>>> onCreateLoader(int id, Bundle args) {
+        // id == HISTORICAL_DATA_LOADER
+        return new HistoricalDataLoader(this, mSymbol);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Pair<Date, Float>>> loader, List<Pair<Date, Float>> data) {
+        plot(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Pair<Date, Float>>> loader) {
+    }
+}
+
--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -1,11 +1,13 @@
 package com.udacity.stockhawk.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.app.ShareCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -29,24 +31,25 @@ import butterknife.ButterKnife;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>,
-        SwipeRefreshLayout.OnRefreshListener,
-        StockAdapter.StockAdapterOnClickHandler {
+    SwipeRefreshLayout.OnRefreshListener,
+    StockAdapter.StockAdapterOnClickHandler {
 
     private static final int STOCK_LOADER = 0;
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.recycler_view)
-    RecyclerView stockRecyclerView;
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.swipe_refresh)
-    SwipeRefreshLayout swipeRefreshLayout;
-    @SuppressWarnings("WeakerAccess")
-    @BindView(R.id.error)
-    TextView error;
+
+    @BindView(R.id.recycler_view) RecyclerView stockRecyclerView;
+    @BindView(R.id.swipe_refresh) SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.error) TextView error;
+
     private StockAdapter adapter;
 
     @Override
     public void onClick(String symbol) {
         Timber.d("Symbol clicked: %s", symbol);
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("symbol", symbol);
+
+        startActivity(intent);
     }
 
     @Override
@@ -86,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private boolean networkUp() {
         ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
         return networkInfo != null && networkInfo.isConnectedOrConnecting();
     }
@@ -134,9 +137,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         return new CursorLoader(this,
-                Contract.Quote.URI,
-                Contract.Quote.QUOTE_COLUMNS.toArray(new String[]{}),
-                null, null, Contract.Quote.COLUMN_SYMBOL);
+            Contract.Quote.URI,
+            Contract.Quote.QUOTE_COLUMNS.toArray(new String[]{}),
+            null, null, Contract.Quote.COLUMN_SYMBOL);
     }
 
     @Override
@@ -159,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private void setDisplayModeMenuItemIcon(MenuItem item) {
         if (PrefUtils.getDisplayMode(this)
-                .equals(getString(R.string.pref_display_mode_absolute_key))) {
+            .equals(getString(R.string.pref_display_mode_absolute_key))) {
             item.setIcon(R.drawable.ic_percentage);
         } else {
             item.setIcon(R.drawable.ic_dollar);
